d2669f8133e85b1b1f67dfb33e3e9d324890ee95
Christina R <xsnachannell@gmail.com>
2022-03-16 01:33:25 -0700

    updates script.js file with attempted solutions for debugging. still not functional with appending buttons

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var timerDiv = document.querySelector("#timer");
 var rankDiv = document.querySelector("#ranking");
 var h1El = document.querySelector("#headingText");
-var quizContent = document.querySelector("#quizBody");
-var pText = document.querySelector("pText");
+var quizContent = document.getElementById("#quizBody");
+var pText = document.querySelector("#pText");
 var leaderBoard;
 var score = 0;
 
@@ -14,10 +14,10 @@ var quizQuestions = {
     question5: ["Question 5", "Option1", "Option2", "Option3", "Option4", "Option1"],
     question6: ["Question 6", "Option1", "Option2", "Option3", "Option4", "Option1"]
 }
-var button1 = document.querySelector("#button1");
-var button2 = document.querySelector("#button2");
-var button3 = document.querySelector("#button3");
-var button4 = document.querySelector("#button4");
+var button1;
+var button2;
+var button3;
+var button4;
 
 var timeLeft = 0;;
 var answer;
@@ -25,86 +25,100 @@ var questionCounter = 0;
 var currentQuestion;
 
 function init() {
-    const easyLvl = document.querySelector("#button1");
-    const medLvl = document.querySelector("#button2");
-    const hardLvl = document.querySelector("#button3");
-    const startQuizButton = document.querySelector("#button4");
-    easyLvl.addEventListener("click", function() {
-        timeLeft = 90;
-        console.log(timeLeft);
-        });
-    medLvl.addEventListener("click", function() {
-        timeLeft = 60;
-        console.log(timeLeft);
-        });
-    hardLvl.addEventListener("click", function() {
-        timeLeft = 45;
-        console.log(timeLeft);
-        });
+    var startQuizButton = document.querySelector("#startQuiz");
     startQuizButton.addEventListener("click", function() {
-        if (timeLeft === 0) {
-            alert("Please select Difficulty Level");
-        } else {
-        startQuiz();
-        return;
-        }
+    createQuestionButtons();
+    startQuizButton.style.display = "none";
+    pText.style.display = "none";
+    setTime();
+    startQuiz();
     });
 
 }
 
+// function init() {
+//     const easyLvl = document.querySelector("#button1");
+//     const medLvl = document.querySelector("#button2");
+//     const hardLvl = document.querySelector("#button3");
+//     const startQuizButton = document.querySelector("#button4");
+//     easyLvl.addEventListener("click", function() {
+//         timeLeft = 90;
+//         console.log(timeLeft);
+//         });
+//     medLvl.addEventListener("click", function() {
+//         timeLeft = 60;
+//         console.log(timeLeft);
+//         });
+//     hardLvl.addEventListener("click", function() {
+//         timeLeft = 45;
+//         console.log(timeLeft);
+//         });
+//     startQuizButton.addEventListener("click", function() {
+//         if (timeLeft === 0) {
+//             alert("Please select Difficulty Level");
+//         } else {
+//         startQuiz();
+//         return;
+//         }
+//     });
+
+// }
+
 function startQuiz() {
     countdown();
     loadQuestion(questionCounter);
-
+    
     button1.addEventListener("click", function() {
         if (currentQuestion[1] === currentQuestion[5]) {
-            questionCounter ++;
-            loadQuestion(questionCounter);
-            pText.textContent = " ";
+            score ++;
         } else {
-            pText.textContent = "Wrong Answer!";
-            pText.style.font = "italic red"
             timeLeft = timeLeft - 5;
-            }
+        }
+        questionCounter ++;
+        loadQuestion(questionCounter);
     });
 
     button2.addEventListener("click", function() {
         if (currentQuestion[2]  === currentQuestion[5]) {
-            questionCounter ++;
             score ++;
-            loadQuestion(questionCounter);
-            pText.textContent = " ";
         } else {
-            pText.textContent = "Wrong Answer!";
-            pText.style.font = "italic red"
             timeLeft = timeLeft - 5;
-            }
+        }
+        questionCounter ++;
+        loadQuestion(questionCounter);
     });
 
     button3.addEventListener("click", function() {
         if (currentQuestion[3]  === currentQuestion[5]) {
-            questionCounter ++;
-            loadQuestion(questionCounter);
-            pText.textContent = " ";
+            score ++;
         } else {
-            pText.textContent = "Wrong Answer!";
-            pText.style.font = "italic red"
             timeLeft = timeLeft - 5;
-            }
+        }
+        questionCounter ++;
+        loadQuestion(questionCounter);
     });
 
     button4.addEventListener("click", function() {
-        if (button4.textContent === currentQuestion[5]) {
-            questionCounter ++;
-            loadQuestion(questionCounter);
-            pText.textContent = " ";
+        if (currentQuestion[4] === currentQuestion[5]) {
+            score ++;
         } else {
-            pText.textContent = "Wrong Answer!";
-            pText.style.font = "italic red"
             timeLeft = timeLeft - 5;
-            }
+        }
+        questionCounter ++;
+        loadQuestion(questionCounter);
     });
    
+}
+
+function createQuestionButtons() {
+    button1 = document.createElement("button");
+    document.getElementById("#quizBody").appendChild(button1);
+    button2 = document.createElement("button");
+    document.getElementById("#quizBody").appendChild(button2);
+    button3 = document.createElement("button");
+    document.getElementById("#quizBody").appendChild(button3);
+    button4 = document.createElement("button");
+    document.getElementById("#quizBody").appendChild(button4);
 }
 
 function loadQuestion(num) {
@@ -138,6 +152,17 @@ function countdown() {
     }, 1000);
 }
 
+function setTime () {
+    let userInput = prompt("Please select Difficulty Level", "easy, medium, or hard");
+    let difLvl = userInput.toUpperCase();
+     if (difLvl === "HARD") {
+        timeLeft = 45;
+    } else if (difLvl === "MEDIUM") {
+        timeLeft = 60;
+    } else {
+        timeLeft = 90;
+    }
+}
 //returns random number within range given
 
 function randomNumber (min,max) {
@@ -146,13 +171,28 @@ function randomNumber (min,max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function loadScore() {
+
+}
+
 
 function endGame() {
 let userName = prompt("Input your initials for our leaderboard!");
 if (userName === " ") {
     userName = prompt("Input your initials for our leaderboard!");
 } else {
-    var userScore = [userName,score];
+    var userScore = [userName, score];
+    var playAgain = confirm("Would you like to take the quiz again? Hit 'Ok' to play or 'cancel' to end");
+    if (playAgain) {
+        score = 0;
+        questionCounter = 0;
+        setTime();
+        startQuiz();
+        
+    } else {
+        var alert = alert("Thank you for playing!");
+    }
+
 }  
 }
 
