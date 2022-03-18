f55f1cb9e506e3be7bd789ecb86ef854fcdfca69
Christina R <xsnachannell@gmail.com>
2022-03-17 22:40:46 -0700

    updates references in README.md, updates JS for leaderboard show/hide, cleans up unnecessary code and updates CSS to look a bit nicer

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This application is designed to be a quick, timed way for you to challenge your basic coding skills. The quiz is comprised of 6 multiple choice coding questions that you must answer correctly within the timeframe you've selected. You can choose between Easy mode - 90 seconds to complete the quiz, Medium mode - 60 seconds to complete it, or Hard - 45 seconds to complete it. Once the time runs out, you'll be able to see your score and ranking on the left hand leader board!
 
-This quiz was mostly coded in Javascript and it was fairly difficult to code it in a way that was responsive to the user's quiz selections while making sure the correct text was shown on the screen. Choosing which text to display when and how, whether it was manipulating the DOM via Javascript or having it already laid out in HTML, was also difficult to think through and code while creating this quiz.
+This quiz was mostly coded in Javascript and it was fairly difficult to code it in a way that was responsive to the user's quiz selections while making sure the correct text was shown on the screen. Choosing which text to display when and how, whether it was manipulating the DOM via Javascript or having it already laid out in HTML, was also difficult to think through and code while laying out and designing this quiz.
 
 Make no mistake though... the hardest part was definitely coming up with the questions (;
 
@@ -19,3 +19,17 @@ Make no mistake though... the hardest part was definitely coming up with the que
 - (Difference Between Const, Var, and Let)[https://www.geeksforgeeks.org/difference-between-var-let-and-const-keywords-in-javascript/]
 
 - (Appending to Body)[https://stackoverflow.com/questions/50186596/how-to-append-multiple-elements-to-a-div]
+
+- (Accessing the Keys of an Object in JS)[https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys]
+
+- (Sorting an Array Alphabetically)[https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort]
+
+- (Pushing New Elements into an Array)[https://www.w3schools.com/jsref/jsref_push.asp]
+
+- (Concatenating Strings)[https://www.w3schools.com/jsref/jsref_concat_string.asp]
+
+- (Changing Style in the DOM)[https://www.w3schools.com/js/js_htmldom_css.asp]
+
+- (Displaying and Hiding Elements)[https://www.w3schools.com/js/tryit.asp?filename=tryjs_visibility]
+
+- (Using setAttribute to define styles in Javascript)[https://www.techiedelight.com/define-multiple-css-attributes-javascript/]
--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 var timerDiv = document.querySelector("#timer");
 var rankList = document.querySelector("#rankingList");
-var h1El = document.querySelector("#headingText");
 var quizBody = document.querySelector("#quizBody");
 var pText = document.querySelector("#pText");
+var scoreList = document.querySelector("#ranking");
 var leaderBoard = [];
-var score = 0;
+
 
 var quizQuestions = {
-    question1: ["Question 1", "Option1", "Option2", "Option3", "Option4", "Option1"],
-    question2: ["Question 2", "Option1", "Option2", "Option3", "Option4", "Option1"],
-    question3: ["Question 3", "Option1", "Option2", "Option3", "Option4", "Option1"],
-    question4: ["Question 4", "Option1", "Option2", "Option3", "Option4", "Option1"],
-    question5: ["Question 5", "Option1", "Option2", "Option3", "Option4", "Option1"],
-    question6: ["Question 6", "Option1", "Option2", "Option3", "Option4", "Option1"]
+    question1: ["What is the difference between 'let', 'var' and 'const'?", "1 - Nothing, they are all used for declaring variables", "2 - var declares a variable that can be used throughout your JS file (following local and global rules), while let and const can only be used in the block in which they are declared and the value of const cannot be changed", "3 - Var is for a variable, let is for a letter and const is for a constant", "4 - Options 2 and 3", "2 - var declares a variable that can be used throughout your JS file (following local and global rules), while let and const can only be used in the block in which they are declared and the value of const cannot be changed"],
+    question2: ["What method is used to turn objects into strings to be stored in localStorage?", "1- .Stringles()", "2- .turnToString", "3- .stringCheeseMe", "4- JSON.stringify()", "4- JSON.stringify()"],
+    question3: ["What code should be inserted here .document.querySelector(__) to select the class 'fancy'?", "1- '.fancy'", "2- '#fancy'", '3- "fancy"', "4- '$fancy'", "1- '.fancy'"],
+    question4: ["What is one of the function that is offered by the Web API to run a timer on our webpage?", "1- setTime()", "2- runTime()", "3- setInterval()", "4- SetInterval()", "3- setInterval()"],
+    question5: ["What method should be used to assign this element <p id='myParagraph'></p> to the variable 'para1'?", "1- var para = document.querySelector('#myParagraph')", "2- var para = document.getElementByID('myParagraph')", "3- var para  = document.get(#myParagraph)", "4- Both Options 1 and 2", "4- Both Options 1 and 2"],
+    question6: ["If I want a function to iterate a certain number of times, what sort of loop should I use?", "1- A Do-While Loop", "2- An Iterating Loop", "3- A Wait-For Loop", "4- A for loop", "4- A for loop"]
 }
+
 var button1;
 var button2;
 var button3;
 var button4;
 var question;
+var feedback;
 
 var timeLeft = 0;;
 var answer;
 var questionCounter = 0;
 var currentQuestion;
+var score = 0;
 
 function init() {
     var startQuizButton = document.querySelector("#startQuiz");
@@ -36,34 +39,6 @@ function init() {
 
 }
 
-// function init() {
-//     const easyLvl = document.querySelector("#button1");
-//     const medLvl = document.querySelector("#button2");
-//     const hardLvl = document.querySelector("#button3");
-//     const startQuizButton = document.querySelector("#button4");
-//     easyLvl.addEventListener("click", function() {
-//         timeLeft = 90;
-//         console.log(timeLeft);
-//         });
-//     medLvl.addEventListener("click", function() {
-//         timeLeft = 60;
-//         console.log(timeLeft);
-//         });
-//     hardLvl.addEventListener("click", function() {
-//         timeLeft = 45;
-//         console.log(timeLeft);
-//         });
-//     startQuizButton.addEventListener("click", function() {
-//         if (timeLeft === 0) {
-//             alert("Please select Difficulty Level");
-//         } else {
-//         startQuiz();
-//         return;
-//         }
-//     });
-
-// }
-
 function startQuiz() {
     countdown();
     loadQuestion(questionCounter);
@@ -71,7 +46,11 @@ function startQuiz() {
     button1.addEventListener("click", function() {
         if (currentQuestion[1] === currentQuestion[5]) {
             score ++;
+            feedback.setAttribute("style", "color: green; font-style: italic");
+            feedback.textContent = "Correct!"
         } else {
+            feedback.setAttribute("style", "color: red; font-style: italic");
+            feedback.textContent = "Wrong!"
             timeLeft = timeLeft - 5;
         }
         questionCounter ++;
@@ -81,7 +60,11 @@ function startQuiz() {
     button2.addEventListener("click", function() {
         if (currentQuestion[2]  === currentQuestion[5]) {
             score ++;
+            feedback.setAttribute("style", "color: green; font-style: italic");
+            feedback.textContent = "Correct!"
         } else {
+            feedback.setAttribute("style", "color: red; font-style: italic");
+            feedback.textContent = "Wrong!"
             timeLeft = timeLeft - 5;
         }
         questionCounter ++;
@@ -89,9 +72,13 @@ function startQuiz() {
     });
 
     button3.addEventListener("click", function() {
-        if (currentQuestion[3]  === currentQuestion[5]) {
+        if (currentQuestion[3] === currentQuestion[5]) {
             score ++;
+            feedback.setAttribute("style", "color: green; font-style: italic");
+            feedback.textContent = "Correct!"
         } else {
+            feedback.setAttribute("style", "color: red; font-style: italic");
+            feedback.textContent = "Wrong!"
             timeLeft = timeLeft - 5;
         }
         questionCounter ++;
@@ -101,7 +88,11 @@ function startQuiz() {
     button4.addEventListener("click", function() {
         if (currentQuestion[4] === currentQuestion[5]) {
             score ++;
+            feedback.setAttribute("style", "color: green; font-style: italic");
+            feedback.textContent = "Correct!"
         } else {
+            feedback.setAttribute("style", "color: red; font-style: italic");
+            feedback.textContent = "Wrong!"
             timeLeft = timeLeft - 5;
         }
         questionCounter ++;
@@ -111,23 +102,24 @@ function startQuiz() {
 }
 
 function createQuestionButtons() {
-    // var buttonDiv = document.createElement("div");
     question = document.createElement("h1");
-    document.querySelector("#displayQuiz").appendChild(question);
+    document.querySelector("#quizDisplay").appendChild(question);
     button1 = document.createElement("button");
-    document.querySelector("#displayQuiz").appendChild(button1);
+    document.querySelector("#quizDisplay").appendChild(button1);
     button2 = document.createElement("button");
-    document.querySelector("#displayQuiz").appendChild(button2);
+    document.querySelector("#quizDisplay").appendChild(button2);
     button3 = document.createElement("button");
-    document.querySelector("#displayQuiz").appendChild(button3);
+    document.querySelector("#quizDisplay").appendChild(button3);
     button4 = document.createElement("button");
-    document.querySelector("#displayQuiz").appendChild(button4);
-    // document.querySelector("#displayQuiz").appendChild(buttonDiv);
+    document.querySelector("#quizDisplay").appendChild(button4);
+    feedback = document.createElement("h2");
+    document.querySelector("#quizDisplay").appendChild(feedback);
 }
 
 function loadQuestion(num) {
     let quiz = Object.keys(quizQuestions);
     if (num === quiz.length) {
+        timerDiv.textContent = "Finished before time end!";
         endQuiz();
     } else {
         currentQuestion = Object.values(quizQuestions)[num];
@@ -138,12 +130,6 @@ function loadQuestion(num) {
         button4.textContent = currentQuestion[4];
     }
 }
-
-// function createButtons (totalButtons) {
-//     for (let i=0; i<totalButtons; i++) {
-//         document.createElement("button");
-//     }
-// }
 
 function countdown() {
     var timeInterval = setInterval(function() {
@@ -157,7 +143,13 @@ function countdown() {
             endQuiz();
             return;
         }
+
+        if (questionCounter === Object.keys(quizQuestions).length) {
+            timerDiv.textContent = "Finished before time end!";
+            return;
+        } else {
         timeLeft--;
+        }
     }, 1000);
 }
 
@@ -172,35 +164,55 @@ function setTime () {
         timeLeft = 90;
     }
 }
-//returns random number within range given
-
-function randomNumber (min,max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
+//Will use random number generator function in future to add randomizing quiz questions so that the same order isn't used each time.
+// function randomNumber (min,max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+// }
 
 function endQuiz() {
-let userName = prompt("Input your initials for our leaderboard!");
-if (userName === " ") {
-    userName = prompt("Input your initials for our leaderboard!");
-} else {
-    var currentUserScore = userName.concat(": ", score);
-    leaderBoard.push(currentUserScore);
-    localStorage.setItem("leaderBoard", JSON.stringify(leaderBoard));
-    var playAgain = confirm("Would you like to take the quiz again? Hit 'Ok' to play or 'cancel' to end");
-    if (playAgain) {
-        score = 0;
-        questionCounter = 0;
-        setTime();
-        startQuiz();
-        loadScore();
-    } else {
-        alert("Thank you for playing!");
-        loadScore();
+    document.querySelector("#quizDisplay").style.display = "none";
+    var scoreDiv = document.createElement("div");
+    scoreDiv.setAttribute("style", "margin: auto; text-align:center; padding: 10%");
+    var endText = document.createElement("h2");
+    if (score > 1) {
+        endText.textContent = "You got " + score + " questions correct!<br>Enter your initials for our leaderboard!";
+    } else if (score === 1) {
+        endText.textContent = "You got " + score + " questions correct!<br>Enter your initials for our leaderboard!";
+    } else {    
+        endText.textContent = "You got " + score + " questions correct!<br>Enter your initials for our leaderboard!";
     }
-    return;
-}  
+    scoreDiv.appendChild(endText);
+    var scoreForm = document.createElement("form");
+    var initialInput = document.createElement("input");
+    scoreForm.appendChild(initialInput);
+    var submitButton = document.createElement("button");
+    submitButton.textContent = "Submit to Leaderboard";
+    scoreForm.appendChild(submitButton);
+    scoreDiv.appendChild(scoreForm);
+    document.body.appendChild(scoreDiv);
+
+    submitButton.addEventListener("click", function(event){
+        event.preventDefault();
+        let userName = initialInput.value.trim();
+        if (userName.value === " " || userName.value === "") {
+            userName = prompt("Please Input your initials for our leaderboard!");
+        } else {
+            var currentUserScore = userName.concat(": ", score);
+            leaderBoard.push(currentUserScore);
+            localStorage.setItem("leaderBoard", JSON.stringify(leaderBoard));
+            var playAgain = confirm("Would you like to take the quiz again? Hit 'Ok' to play or 'cancel' to end");
+            if (playAgain) {
+                location.reload();
+            } else {
+                alert("Thank you for playing!");
+                loadScore();
+            }
+            return;
+        }  
+    })
+
 }
 
 function loadScore() {
@@ -211,6 +223,23 @@ for (var i=0; i < displayLeaderBoard.length; i++) {
     rankItem.textContent = displayLeaderBoard[i];
     rankList.appendChild(rankItem);
 }
+document.querySelector("#rankingList").style.visibility = "hidden";
+}
+
+function displayScores() {
+    scoreList.addEventListener("click", function() {
+        if (rankingList.getAttribute("data-state") === "hidden") {
+            ranking.textContent = "Hide Leaderboard:"
+            rankingList.setAttribute("data-state", "visible");
+            document.querySelector("#rankingList").style.visibility = "visible";
+        } else {
+            document.querySelector("#rankingList").style.visibility = "hidden";
+            ranking.textContent = "Show Leaderboard:"
+            rankingList.setAttribute("data-state", "hidden");
+        }
+     })
 }
 
 init();
+loadScore();
+displayScores();
